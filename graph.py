8bb4cecf7c0f8b39df2ff715289e7e648fa56f3e
filipposchr <philipp960@gmail.com>
2026-07-02 15:46:05 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -2,64 +2,32 @@ import functools
 import numpy as np
 import torch
 import multiprocessing
-import numba
-from numba import njit, jit
 
 class NeighborFinder:
-    def __init__(self, adj_list, adj_list_out=None, uniform=False):
+    def __init__(self, adj_list, uniform=False):
         """
         Params
         ------
-        adj_list: List[List[Tuple[int, int, float]]]
-            The adjacency list for in-neighbors.
-
-        adj_list_out: Optional[List[List[Tuple[int, int, float]]]]
-            The adjacency list for out-neighbors. Optional.
-
-        uniform: bool
-            Whether to sample uniformly when selecting neighbors.
+        node_idx_l: List[int]
+        node_ts_l: List[int]
+        off_set_l: List[int], such that node_idx_l[off_set_l[i]:off_set_l[i + 1]] = adjacent_list[i]
         """
 
-        # Initialize IN neighbors
         node_idx_l, node_ts_l, edge_idx_l, off_set_l = self.init_off_set(adj_list)
         self.node_idx_l = node_idx_l
         self.node_ts_l = node_ts_l
         self.edge_idx_l = edge_idx_l
+
         self.off_set_l = off_set_l
 
         self.uniform = uniform
 
-        # Initialize OUT neighbors if provided
-        if adj_list_out is not None:
-            (
-                node_idx_l_out,
-                node_ts_l_out,
-                edge_idx_l_out,
-                off_set_l_out
-            ) = self.init_off_set(adj_list_out)
-
-            self.node_idx_l_out = node_idx_l_out
-            self.node_ts_l_out = node_ts_l_out
-            self.edge_idx_l_out = edge_idx_l_out
-            self.off_set_l_out = off_set_l_out
-        else:
-            # If no out-adjacency list provided, set attributes to None
-            self.node_idx_l_out = None
-            self.node_ts_l_out = None
-            self.edge_idx_l_out = None
-            self.off_set_l_out = None
-
     def init_off_set(self, adj_list):
         """
-        Converts adjacency list into flat arrays for fast lookup.
-
         Params
         ------
-        adj_list: List[List[Tuple[int, int, float]]]
+        adj_list: List[List[int]]
 
-        Returns
-        -------
-        node_idx_l, node_ts_l, edge_idx_l, off_set_l
         """
         n_idx_l = []
         n_ts_l = []
@@ -91,7 +59,7 @@ class NeighborFinder:
         src_ngh_t_batch_flat = src_ngh_t_batch.flatten()  # reshape(batch_size, -1)
         self.preprocess(tuple(src_ngh_node_batch_flat), tuple(src_ngh_t_batch_flat), layer - 1, num_neighbors)
 
-    #@functools.lru_cache(maxsize=None, typed=True)
+    @functools.lru_cache(maxsize=None, typed=True)
     def find_before(self, src_idx, cut_time):
         """
 
@@ -125,7 +93,7 @@ class NeighborFinder:
         else:
             return neighbors_idx[:left], neighbors_ts[:left]
 
-    #@functools.lru_cache(maxsize=None, typed=True)
+    @functools.lru_cache(maxsize=None, typed=True)
     def get_temporal_neighbor(self, src_idx_l, cut_time_l, num_neighbors=20):
         """
         Params
@@ -189,87 +157,6 @@ class NeighborFinder:
                 indice = num_neighbors - len(ngh_idx)
                 out_ngh_node_batch[i, indice:] = ngh_idx
                 out_ngh_t_batch[i, indice:] = ngh_ts
-
-        return out_ngh_node_batch, out_ngh_t_batch
-
-    def find_before_out(self, src_idx, cut_time):
-        """
-        Find out-neighbors of src_idx occurring at or after cut_time.
-        """
-        if self.node_idx_l_out is None:
-            raise RuntimeError("Out adjacency list not loaded. Pass adj_list_out in the constructor.")
-
-        node_idx_l = self.node_idx_l_out
-        node_ts_l = self.node_ts_l_out
-        off_set_l = self.off_set_l_out
-
-        neighbors_idx = node_idx_l[off_set_l[src_idx]:off_set_l[src_idx + 1]]
-        neighbors_ts = node_ts_l[off_set_l[src_idx]:off_set_l[src_idx + 1]]
-
-        if len(neighbors_ts) == 0:
-            return np.array([], dtype=int), np.array([], dtype=float)
-
-        left = 0
-        right = len(neighbors_ts)
-
-        while left < right:
-            mid = (left + right) // 2
-            if neighbors_ts[mid] < cut_time:
-                left = mid + 1
-            else:
-                right = mid
-
-        # neighbors_ts[left:] should all be ≥ cut_time
-        neighbors_idx = neighbors_idx[left:]
-        neighbors_ts = neighbors_ts[left:]
-
-        # filter just in case
-        mask = neighbors_ts >= cut_time
-        neighbors_idx = neighbors_idx[mask]
-        neighbors_ts = neighbors_ts[mask]
-
-        return neighbors_idx, neighbors_ts
-
-    #@functools.lru_cache(maxsize=None, typed=True)
-    def get_temporal_out_neighbor(self, src_idx_l, cut_time_l, num_neighbors=20):
-        if self.node_idx_l_out is None:
-            raise RuntimeError("Out adjacency list not loaded. Pass adj_list_out in the constructor.")
-
-        assert (len(src_idx_l) == len(cut_time_l))
-
-        out_ngh_node_batch = np.full((len(src_idx_l), num_neighbors), 0, dtype=np.int32)
-
-        cut_time_l_arr = np.array(cut_time_l, dtype=np.float32)
-        out_ngh_t_batch = np.full(
-            (len(src_idx_l), num_neighbors),
-            cut_time_l_arr[:, None] + 1,
-            dtype=np.float64
-        )
-
-        for i, (src_idx, cut_time) in enumerate(zip(src_idx_l, cut_time_l)):
-            ngh_idx, ngh_ts = self.find_before_out(src_idx, cut_time)
-
-            if len(ngh_idx) > 0:
-                if len(ngh_idx) > num_neighbors:
-                    degree_batch = self.off_set_l_out[ngh_idx + 1] - self.off_set_l_out[ngh_idx]
-                    neighbors = np.argsort(degree_batch)[-num_neighbors:]
-                    ngh_idx = ngh_idx[neighbors]
-                    ngh_ts = ngh_ts[neighbors]
-
-                pad_len = num_neighbors - len(ngh_idx)
-                if pad_len > 0:
-                    pad_idx = np.zeros(pad_len, dtype=np.int32)
-                    pad_ts = np.full(pad_len, cut_time + 1, dtype=np.float32)
-                    ngh_idx = np.concatenate([pad_idx, ngh_idx])
-                    ngh_ts = np.concatenate([pad_ts, ngh_ts])
-
-                out_ngh_node_batch[i, :] = ngh_idx
-                out_ngh_t_batch[i, :] = ngh_ts
-
-            else:
-                out_ngh_node_batch[i, :] = 0
-                out_ngh_t_batch[i, :] = cut_time + 1
-
 
         return out_ngh_node_batch, out_ngh_t_batch
 
