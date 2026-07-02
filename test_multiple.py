62fa113d655f4b0c059dc6d097dd4c09e072e323
filipposchr <philipp960@gmail.com>
2026-07-02 15:42:43 +0300

    Update test_multiple.py

--- test_multiple.py
+++ test_multiple.py
@@ -1,27 +1,15 @@
-import argparse
 import subprocess
+    
+datasets = ["edit-haggle", "edit-SFHH", "edit-observational", "edit-sp-hypertext", "edit-sp-workplace", \
+             "edit-hs2011", "edit-hs2012","edit-hs2013" \
+            , "edit-ia-reality-call", "edit-infectious", "edit-sewiki",\
+            "edit-company-emails", "edit-ants1-1", "edit-ants1-2",\
+             "edit-ants2-1", "edit-ants2-2",
+              "edit-eu-email-2", "edit-eu-email-3", "edit-eu-email-4", "edit-sp-hospital"]
 
-datasets = [ "edit-mathoverflow",  "edit-facebook_wall","edit-topology" ,  "edit-mlwikiquote", "edit-plwikiquote", "edit-digg_reply", "edit-SMS", "edit-rt-pol", "edit-slashdot_reply","edit-wamazon", "edit-mgwikipedia", "edit-tgwiktionary", "edit-ltwiktionary"]
-#datasets = [ "edit-infectious","edit-facebook_wall" ]
-# Argument parser
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--bet", choices=["sh", "sfm"], help="Choose betweenness mode: 'sh' or 'sfm'")
-group.add_argument("--close", choices=["f", "sh"], help="Choose closeness mode: 'f' or 'sh'")
-
-args = parser.parse_args()
-
-if args.bet is not None:
-    mode_flag = "--bet"
-    mode_value = args.bet
-else:
-    mode_flag = "--close"
-    mode_value = args.close
 
 for d in datasets:
-    print(f"\n - Running evaluation for dataset: *{d}* -")
-    command = ["python", "-u", "main.py", "-d", d, mode_flag, mode_value, "--test"]
+    command = ["python", "-u", "main_28oct.py", "-d", d, "--test"]
     result = subprocess.run(command)
-
     if result.returncode != 0:
         print(f"Error: Evaluation failed for dataset {d}")
